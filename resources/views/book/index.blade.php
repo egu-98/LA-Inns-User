86277aeab5f43fdb42807116d8c6234058b12279
egu-98 <2021php41-student012@la-study.com>
2021-05-24 16:48:26 +0900

    add book function

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -17,22 +17,36 @@
     <form action="{{ route( 'books.create' ) }}" method="GET" >
         @csrf
         <p>
-            <label for="name">チェックイン</label>
+            <label for="name">チェックイン：</label>
             <input type="date" name="checkin_date">
         </p>
         <p>
-            <label for="name">チェックアウト</label>
+            <label for="name">チェックアウト：</label>
             <input type="date" name="checkout_date">
         </p>
         <p>
-            <label for="plan">プラン</label>
+            <label for="plan">プラン：</label>
             <select name="plan">
                 @foreach( $plans as $plan )
-                    <option value="{{ $plan->id }}">{{ $plan->name }}</option>
+                    <option value="{{ $plan->id }}" id="plan_{{ $plan->id }}" >{{ $plan->name }}: {{ $plan->price }}円/泊</option>
                 @endforeach
             </select>
         </p>
-        <button type="submit">変更する</button>
+        <p>
+            <label for="rooms" id="rooms">部屋数：</label>
+            <input type="text" name="rooms" style="width: 5em">部屋
+        </p>
+
+        <hr>
+
+        <div>
+            料金：<span id="total_price"></span>
+            <script>
+                
+            </script>
+        </div>
+        
+        <button type="submit">予約する</button>
     </form>
 </div>
 @endsection

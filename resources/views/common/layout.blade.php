4d70bbf62bf5999d085108298c533f578e44e32e
egu-98 <2021php41-student012@la-study.com>
2021-05-25 17:16:22 +0900

    fixed home layout

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -7,43 +7,55 @@
     <title>{{ config( 'app.name' ) }}</title>
 
     <link rel="stylesheet" href="{{ asset( 'css/layout.css' ) }}">
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.6.22/dist/css/uikit.min.css" />
+
+    <!-- UIkit JS -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.22/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.22/dist/js/uikit-icons.min.js"></script>
 </head>
 <body>
     <div>
-        <div>
-            <img src="{{ asset( 'img/LA_Inns.jpg' ) }}" alt="LA_Inns" style="width: 11ex; height: 14ex" onclick="location.href='/'">
-        </div>
-        <div>
+        <div class="uk-margin-remove-bottom uk-child-width-expand@s" uk-grid>
+            <div>
+                <img src="{{ asset( 'img/LA_Inns.jpg' ) }}" alt="LA_Inns" style="width: 10ex; height: 13ex" onclick="location.href='/'">
+            </div>
+
             @yield( 'header' )
+            
+            <div class="uk-text-right uk-margin-small-right uk-margin-top">
+                @if( Auth::check() )
+                <div class="uk-inline">
+                    <button class="uk-button uk-button-default" type="button">アカウント</button>
+                    <div uk-dropdown="mode: click" class="uk-text-left">
+                        <p>
+                            <a href="{{ route( 'users.index' ) }}">アカウント情報</a>
+                        </p>
+                        <p>
+                            <a href="" onclick="logout()">ログアウト</a>
+                        </p>
+                    </div>
+                    <form action="{{ route( 'logout' ) }}" method="POST" id="logout-form">
+                        @csrf
+                   </form>
+                   <script type="text/javascript">
+                       function logout(){
+                           event.preventDefault();
+                           if( window.confirm( 'ログアウトしますか？' ) ){
+                               document.getElementById( 'logout-form' ).submit();
+                           }
+                       }
+                   </script>
+                </div>    
+                @else
+                    <a class="uk-button uk-button-default" type="button" href="{{ route( 'login' ) }}">ログイン</a>
+                @endif
+            </div>
         </div>
-        <div>
-            @if( Auth::check() )
-                <ul class="navigation">
-                    <li>
-                        <a href="{{ route( 'users.index' ) }}">アカウント</a>
-                    </li>
-                    <li>
-                        <a href="" onclick="logout()">ログアウト</a>
-                        <form action="{{ route( 'logout' ) }}" method="POST" id="logout-form">
-                            @csrf
-                        </form>
-                        <script type="text/javascript">
-                            function logout(){
-                                event.preventDefault();
-                                if( window.confirm( 'ログアウトしますか？' ) ){
-                                    document.getElementById( 'logout-form' ).submit();
-                                }
-                            }
-                        </script>
-                    </li>
-                </ul>
-            @else
-                <a class="button" href="{{ route( 'login' ) }}">ログイン</a>
-            @endif
+        
         </div>
-    </div>
-    
-    <hr>
+  
+    <hr class="uk-margin-remove-top">
 
     <div>
         @yield( 'contents' )
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,36 @@
 @extends( 'common.layout' )
 
 @section( 'header' )
-<h1>宿を探す</h1>
+<h1 class="uk-text-center uk-margin-top">宿を探す</h1>
 @endsection
 
 @section( 'contents' )
-<div>
-    <form action="{{ route( 'inn_search' ) }}" method="GET">
+<div class="uk-text-center uk-margin-bottom">
+    <form class="uk-search uk-search-large" action="{{ route( 'inn_search' ) }}" method="GET" style="border: 1px solid; boder-color: gray; border-radius: 20px;">
         @csrf
-        <input  type="search" name="address" placeholder="ロケーション" value="{{ request( 'location' ) }}">
-        <button type="submit">検索</button>
+        <input class="uk-search-input" type="search" name="address" placeholder="ロケーション" value="{{ request( 'location' ) }}">
+        <button class="uk-search-icon-flip" type="submit" uk-search-icon></button>
     </form>    
 </div>
 
-<div>
-    @if( isset( $inns ) )
-        @foreach( $inns as $inn )
-            <div>
-                <div>
-                    <img src="{{ asset( 'img/inn_pic.jpeg' ) }}" alt="{{ $inn->name }}_pic" style="width: 33ex; height: 20ex">
-                    <p>{{ $inn->name }}</p>
+
+<div class="uk-margin-xlarge-left uk-margin-xlarge-right">
+@if( isset( $inns ) )
+    @foreach( $inns as $inn )
+        <div class="uk-card uk-card-default uk-card-body uk-margin-bottom">
+            <div class="uk-flex">
+                <div class="uk-container uk-margin-remove">
+                    <img src="{{ asset( 'img/inn_pic.jpeg' ) }}" alt="{{ $inn->name }}_pic" style="width: 40ex; height: 25ex">
+                </div>
+                <div class="uk-container uk-margin-remove">
+                    <h2>{{ $inn->name }}</h2>
+                    <p>{{ $inn->address }}</p>
                     <a href="{{ route( 'inns.show', $inn ) }}">詳細を見る</a>
                 </div>
             </div>
-        @endforeach    
-    @endif
+        </div>
+    @endforeach    
+@endif
 </div>
+
 @endsection
